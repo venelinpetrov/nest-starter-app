0566820fd5b3d6b2b4bf6367b8bd7bc1f0186ead
venelinpetrov <venelin.petrov@cubbystorage.com>
2024-08-12 18:42:28 +0300

    Add port const

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 9000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+
+  console.log('Server started at port: ', PORT);
 }
 bootstrap();
