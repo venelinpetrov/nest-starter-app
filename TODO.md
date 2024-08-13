fb6ba6308535ac95b478248703905fbb6fcefa8e
venelinpetrov <venelin.petrov@cubbystorage.com>
2024-08-13 15:08:51 +0300

    Setup Prisma with sqlite. Sample services and controller

---
+++ TODO.md
@@ -0,0 +1,16 @@
+1. Setup
+    - [done] Start NestJS project
+    - Integrate with Prisma
+    - REST API wiwth 1 CRUD controller
+    - Authentication (JWT)
+    - Authorization (Permissions, Roles)
+    - Open API / Swagger UI
+
+2. Setup database (e.g. MySQL, SQLite)
+
+3. Setup FE
+    - React with TS
+    - Redux toolkit (RTK)
+    - React Router
+    - MUI
+    - eslint: lint, autofix, config (single quotes, 4 spaces etc.)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PostService } from './post.service';
+import { UserService } from './user.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostService, UserService, PrismaService],
 })
 export class AppModule {}
